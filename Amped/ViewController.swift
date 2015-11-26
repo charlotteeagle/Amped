30955f960ee20fe20f5c6f5a3ec8277224400a71
Charlotte Eagle (i7676522) <i7676522@mac27775.local>
2015-11-26 10:02:45 +0000

    Initial Commit

---
+++ Amped/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amped
+//
+//  Created by Charlotte Eagle (i7676522) on 26/11/2015.
+//  Copyright © 2015 Charlotte Eagle (i7676522). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
