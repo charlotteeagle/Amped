53fb99cc518e07b368c411319cfee15abd8d0501
Charlotte Eagle (i7676522) <i7676522@mac27775.local>
2015-12-01 10:41:55 +0000

    Initial commit

--- Amped/ViewController.swift
+++ Amped/ViewController.swift
@@ -1,25 +1,45 @@
-//
-//  ViewController.swift
-//  Amped
-//
-//  Created by Charlotte Eagle (i7676522) on 26/11/2015.
-//  Copyright © 2015 Charlotte Eagle (i7676522). All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        data.append("Kyle")
     }
 
 
 }
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")
+        
+        let story = data[indexPath.row]
+        
+        let titleLabel = cell?.viewWithTag(1) as! UILabel
+        titleLabel.text = story
+        
+        return cell!
+    }
+    
+}
+
+
+extension ViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+}
+
 
