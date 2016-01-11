4d85ae34b52ffba4f52a4569a59282ef6c2225d6
charlotteeagle <i7676522@bournemouth.ac.uk>
2016-01-11 13:43:03 +0000

    colour 2

--- Amped/Story.swift
+++ Amped/Story.swift
@@ -18,15 +18,12 @@ class Story {
         date = json["data"].intValue
         
         if json["color"].intValue == 1 {
-            color = UIColor(r: 132, g: 202, b: 173, a: 2)
+            color = UIColor(r: 197, g: 215, b: 232, a: 2)
         } else if json["color"].intValue == 2 {
-            color = UIColor.redColor()
+            color = UIColor(r: 255, g: 226, b: 213, a: 2)
         }  else if json["color"].intValue == 3 {
-            color = UIColor.greenColor()
-        
+            color = UIColor(r: 255, g: 238, b:213, a: 2)
         
     }
-    
-    
 }
 }
